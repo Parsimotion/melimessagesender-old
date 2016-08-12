0ecc705c018757184421b6540e0c090d44d8e9f8
Rodrigo Alfonso <rodri042@gmail.com>
2016-08-12 09:59:37 -0300

    Agregando template de ruta para poder llamar al action con el user id

--- MeliMessageSender/Controllers/MerchantOrdersNotificationsController.cs
+++ MeliMessageSender/Controllers/MerchantOrdersNotificationsController.cs
@@ -6,31 +6,38 @@ using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace MeliMessageSender.Controllers
 {
-	[RoutePrefix("/notifications/merchantorders")]
+	//[RoutePrefix("/notifications/merchantorders")]
 	public class MerchantOrdersNotificationsController : NotificationsController
 	{
 		public MerchantOrdersNotificationsController(CloudQueue cloudQueue) : base(cloudQueue)
 		{
 		}
 
-		[Route("{userId}"), HttpPost]
-		public IHttpActionResult MerchantOrderMessage(long userId)
+		//[Route("{userId}"), HttpPost]
+		public IHttpActionResult Post(long id)
 		{
+			var merchantOrderId = GetMerchantOrderId();
+			if (string.IsNullOrEmpty(merchantOrderId)) return this.Ok();
+
 			var value = new
 			{
 				resource = "/merchantorders/" + GetMerchantOrderId(),
-				user_id = userId,
+				user_id = id,
 				topic = "merchantorders"
 			};
+
 			return this.LogValueAndAddMessage(value);
 		}
 
 		private string GetMerchantOrderId()
 		{
-			return Request.GetQueryNameValuePairs()
-				.Where(param => param.Key == "merchant_order")
+			var queryString = Request.GetQueryNameValuePairs();
+			if (!queryString.Any(param => param.Key == "topic" && param.Value == "merchant_order")) return "";
+
+			return queryString
+				.Where(param => param.Key == "id")
 				.Select(param => param.Value)
-				.FirstOrDefault();
+				.First();
 		}
 	}
 }
